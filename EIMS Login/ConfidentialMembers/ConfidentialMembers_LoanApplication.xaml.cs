17878c5f1dac57c62b245f85ea8344325c1439b3
shuaicheng <shuaicheng@>
2016-05-25 19:04:29 +0800

    哈哈

--- EIMS Login/ConfidentialMembers/ConfidentialMembers_LoanApplication.xaml.cs
+++ EIMS Login/ConfidentialMembers/ConfidentialMembers_LoanApplication.xaml.cs
@@ -24,5 +24,10 @@ namespace EIMS_Login
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
