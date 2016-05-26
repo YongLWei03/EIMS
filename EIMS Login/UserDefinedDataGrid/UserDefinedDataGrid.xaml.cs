90b22a61bad6fa05dd12f11b35116a3170989b2a
shuaicheng <shuaicheng@>
2016-05-26 22:57:21 +0800

    ggg

--- EIMS Login/UserDefinedDataGrid/UserDefinedDataGrid.xaml.cs
+++ EIMS Login/UserDefinedDataGrid/UserDefinedDataGrid.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace EIMS_Login.UserDefinedDataGrid
 {
@@ -20,9 +22,45 @@ namespace EIMS_Login.UserDefinedDataGrid
     /// </summary>
     public partial class UserDefinedDataGrid : UserControl
     {
+        Connection Temp = new Connection();
         public UserDefinedDataGrid()
         {
             InitializeComponent();
+            dataGrid.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            dataGrid.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+        }
+        public void InitTableHeightWidth(int Height,int Width)
+        {
+            dataGrid.Height = Height;
+            dataGrid.Width = Width;
+            dataGrid.FontSize = 16;
+            
+        }
+        public void AddColumns(string Binding,string Header,int Width)
+        {
+            dataGrid.AutoGenerateColumns = false;
+            System.Windows.Data.Binding binding = new System.Windows.Data.Binding(Binding);
+            binding.Mode = System.Windows.Data.BindingMode.OneWay;
+            DataGridTextColumn Temp = new DataGridTextColumn();
+            Temp.Header = Header;
+            Temp.Width = Width;
+            Temp.Binding = binding;
+            Temp.ElementStyle = Resources["MyDataGrid"] as Style;
+            dataGrid.Columns.Add(Temp);
+        }
+        public void DataTableSelect(string SQL)
+        {
+            DataTable Tabel0 = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(SQL, Temp.GetConnStr());
+            DataSet ds = new DataSet();
+            ds.Clear();
+            sda.Fill(Tabel0);
+            dataGrid.ItemsSource = Tabel0.DefaultView;       
+        }
+
+        private void dataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex() + 1;
         }
     }
 }
