20852a831ab86714f490fd47701c69ff1874b493
guopeng1996 <154771319@qq.com>
2016-06-06 13:19:50 +0800

    123456

---
+++ EIMS Login/PersonalInformation.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace EIMS_Login
+{
+    /// <summary>
+    /// PersonalInformation.xaml 的交互逻辑
+    /// </summary>
+    public partial class PersonalInformation : UserControl
+    {
+        private byte[] _imageBinary;
+        private string _imgLocalPath;
+
+        public PersonalInformation()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "PNG File(*.png)|*.png"; ;
+            dialog.Multiselect = false;
+
+            if (dialog.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(dialog.FileName, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                this._imageBinary = br.ReadBytes((int)fs.Length);
+                this._imgLocalPath = dialog.FileName;
+                br.Close();
+                fs.Close();
+                this.MyImage.Source = ByteArrayToBitmapImage(this._imageBinary);
+            }
+        }
+        BitmapImage ByteArrayToBitmapImage(byte[] byteArray)
+        {
+            BitmapImage bmp = null;
+
+            try
+            {
+                bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = new MemoryStream(byteArray);
+                bmp.EndInit();
+            }
+            catch
+            {
+                bmp = null;
+            }
+
+            return bmp;
+        }
+    }
+}
